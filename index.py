0b1643d393ea7b29a5aee6850c7a25d04dc57078
ian speelman <ianspeelman@gmail.com>
2025-01-20 19:48:57 +0100

    initial commit

---
+++ index.py
@@ -0,0 +1,8 @@
+import urllib.request,os
+
+api_key = os.environ['API_KEY']
+api_secret = os.environ['API_SECRET']
+api_endpoint = os.environ['API_ENDPOINT']
+print(api_key)
+print(api_secret)
+print(api_endpoint)
